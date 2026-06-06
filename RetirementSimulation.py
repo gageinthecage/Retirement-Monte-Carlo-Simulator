b5a471452f6438f50a5e2f5ec2c0d4d3a06e2471
gageinthecage <gagewboyd@gmail.com>
2026-06-06 13:12:11 -0700

    Refactor retirement simulation with updated rates and logic
    Updated the retirement simulation algorithm with new fixed rates and improved the simulation logic to track portfolio paths.

--- RetirementSimulation.py
+++ RetirementSimulation.py
@@ -1,6 +1,9 @@
-import numpy as np
+#The algorithm that generates the simulated SMP returns#
 
-#16.76 = annulized volatility & 8.5% returns
+import numpy as np
+import statistics
+
+#Blach Scholes model that generates a random growth pattern#
 
 def  S_T(r,o,t):
     Z = np.random.randn()
@@ -10,21 +13,53 @@ def  S_T(r,o,t):
 #Target is the amount of money the consumer wants, portfolio is the updating money gained#
 
 def Retire_Simulate(simulations, target, starting, monthly):
-    r = .085
-    o = .167
+#Fixed rates from Annual SMP data#
+    simulations = 2000
+    r = 0.105
+    o = 0.175
     t = 1/12
+
     months = []
+    paths = []
+
     for i in range(simulations):
         m = 0
         portfolio = starting
-        months.append(m)
-        while portfolio < target and m < 9600:
+        path = [portfolio]
+        while portfolio < target and m < 720:
+
+#Random growth applied to the portfolio and monthly contribution added#
+        
             growth = S_T(r,o,t)
             portfolio = portfolio * growth
             portfolio = portfolio + monthly
             m = m + 1
-    return sum(months) / len(months)
+            path.append(portfolio)
+        months.append(m)
+        paths.append(path)
 
+# Finding Median and Quartiles
 
+    median = statistics.median(months)
+    sort = sorted(months)
+    t1 = sort[int(.9*len(months))]
+    b1 = sort[int(.1*len(months))]
 
+# Finding Success rate Percentage
 
+    success_rate=(sum(1 for m in months if m < 720)/len(months)) * 100
+
+    simulations = 2000
+
+    average = sum(months)/len(months)
+
+    return {
+        "avg_months": average,
+        "median_months": median,
+        "num_simulations": simulations,
+        "success_rate": success_rate,
+        "bottom_10_months": b1,
+        "top_10_months": t1,
+        "all_months": months,
+        "all_paths": paths  # frontend uses this for plotting
+    }
